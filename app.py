761ced26ebdedbf4f308f02dbe34dc16121ff1a6
Quan Quan <kinensake>
2021-11-20 15:57:25 +0700

    Dung code structure

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+from configs.db import getDB
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
+
+# get DB
+db = getDB()
+
+# static files
+app.mount("/public", StaticFiles(directory="public"))
 
 @app.get("/")
 def main():
-	return {"data": "hello world"}
+	return {"data": db}
