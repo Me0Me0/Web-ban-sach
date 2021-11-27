4808cb6936ddee6aaf068f202f9dce52fc0e89f1
Quan Quan <kinensake>
2021-11-27 15:57:11 +0700

    add signup route

--- public/js/signin/main.js
+++ public/js/signin/main.js
@@ -13,15 +13,41 @@ if (formLogin.attachEvent) {
     formLogin.addEventListener('submit', onFormsubmit);
 }
 
-function onFormsubmit(e) {
+async function onFormsubmit(e) {
     e.preventDefault();
     var user = inputUsername.value;
     var pass = inputPassword.value;
 
-    if (user == CORRECT_USER && pass == CORRECT_PASS) {
-        alert('Đăng nhập thành công');
-    } else {
+    if (pass.length < 8) {
+        alert('Mật khẩu phải có ít nhất 8 kí tự');
+    }
+
+    // if (user == CORRECT_USER && pass == CORRECT_PASS) {
+    //     alert('Đăng nhập thành công');
+    // } else {
+    //     alert('Tài khoản hoặc mật khẩu không đúng');
+    // }
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            username: user,
+            password: pass
+        })
+    }
+
+    const res = await fetch("/users/signin", options);
+    const data = await res.json();
+
+    if (data.error) {
         alert('Tài khoản hoặc mật khẩu không đúng');
+    } else if (data.data.success) {
+        alert('Đăng nhập thành công');
+        window.location.href = "/";
+    } else {
+        alert('Đã xảy ra lỗi, vui lòng thử lại sau');
     }
 }
 
