2c289c2994ba44a0d004f4578dbffc321b064d89
gftrftrdfr <68469054+gftrftrdfr@users.noreply.github.com>
2022-01-05 20:19:21 +0700

    trang đổi mk

--- public/js/changePassword/main.js
+++ public/js/changePassword/main.js
@@ -1,19 +1,33 @@
-var usernameInput = document.getElementById('username');
+var oldPassword = document.getElementById('oldpass');
+var passwordInput = document.getElementById('pass');
+var repasswordInput = document.getElementById('repass')
+var formChangePassword = document.getElementById('change_password');
+var token = window.location.href;
+var expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/;
 
-var formForgotPassword = document.getElementById('forgot_password');
-
-if (formForgotPassword.attachEvent) {
-    formForgotPassword.attachEvent('submit', onFormsubmit);
+if (formChangePassword.attachEvent) {
+    formChangePassword.attachEvent('submit', onFormsubmit);
 } else {
-    formForgotPassword.addEventListener('submit', onFormsubmit);
+    formChangePassword.addEventListener('submit', onFormsubmit);
 }
 
 async function onFormsubmit(e) {
     e.preventDefault();
-    var username = usernameInput.value;
+    var oldpass = oldPassword.value;
+    var password = passwordInput.value;
+    var repassword = repasswordInput.value;
 
-    if (username.length < 5) {
-        alert('Tên đăng nhập phải có ít nhất 5 ký tự')
+    if (password.length < 8) {
+        alert('Mật khẩu phải có ít nhất 8 ký tự');
+    }
+    else if(!expression.test(password)){
+        alert('Mật khẩu phải chứa ít nhất 1 chữ hoa, 1 chữ thường, 1 số và không có ký tự đặc biệt')
+    }
+    else if(repassword==''){
+        alert('Vui lòng xác nhận lại mật khẩu');
+    }
+    else if(repassword!==password){
+        alert('Mật khẩu xác nhận không trùng khớp');
     }
     else
     {
@@ -23,18 +37,20 @@ async function onFormsubmit(e) {
                 "content-type": "application/json"
             },
             body: JSON.stringify({
-                username: username
+                oldpass: oldpass,
+                newpass: password
             })
-        }        
-
-        const res = await fetch("/users/forgot-password", options);
+        }   
+        
+        const res = await fetch("/users/change-password", options);
         const data = await res.json();
 
-        //Có gì sửa lại dòng này giúp e
+
         if (data.error) {
-            alert('Tên đăng nhập không tồn tại');
-        } else if (data.data.status_code == 202) {
-            alert('Liên kết dẫn đến trang đặt lại mật khẩu đã gửi tới email đăng ký tài khoản này');
+            alert('Đổi mật khẩu thất bại');
+        } else if (data.data.success) {
+            alert('Đổi mật khẩu thành công, đăng nhập để tiếp tục sử dụng');
+            location.href = "/users/signin"
         } else {
             alert('Đã xảy ra lỗi, vui lòng thử lại sau');
         }
