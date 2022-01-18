f4bb8ffd758e92c99fcf00f5f501656714b63039
minhnhathcmus <leonidas53664327@gmail.Com>
2022-01-18 14:19:33 +0700

    fix hiển thị thêm " VND" ở new-product

--- public/js/newProduct/api.js
+++ public/js/newProduct/api.js
@@ -61,7 +61,7 @@ async function showProducts() {
 
         clone.querySelector("div").id = product.id;
         clone.querySelector(".product-name").textContent = product.name;
-        clone.querySelector(".product-price").textContent = product.price;
+        clone.querySelector(".product-price").textContent = product.price + " VND";
         clone.querySelector(".product-image").src = product.cover_image;
         clone.querySelector("#product-detail").href = '/products/' + product.id;
 
